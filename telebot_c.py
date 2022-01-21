e50eb9707c2f8cf30fe239b895ab3106b63319af
BilenkoAnton <bilenko.anton@gmail.com>
2022-01-22 01:22:00 +0200

    21.01.2022
    some update

--- telebot_c.py
+++ telebot_c.py
@@ -6,9 +6,9 @@ import sqlite3
 
 TOKEN = '5132273473:AAEkJ6BWsVUXjpp5o8mfRqGnp1AsnYSBluk'
 bot = telebot.TeleBot(TOKEN)
-last_function = datetime.datetime.now() - datetime.timedelta(days=1)
 connection = sqlite3.connect('../todo_app/todo/db.sqlite3')
 cursor = connection.cursor()
+last_function = datetime.datetime.utcnow() - datetime.timedelta(days=1)
 
 
 def get_telegram_and_user_id_list():
@@ -19,7 +19,8 @@ def get_telegram_and_user_id_list():
 def get_messages(user_id):
     messages = list(cursor.execute(f'SELECT text FROM todo_app_message WHERE account_id = {user_id} AND '
                                    f'message_date BETWEEN "{last_function}" '
-                                   f'AND "{datetime.datetime.now()-datetime.timedelta(hours=2)}"'))
+                                   f'AND "{datetime.datetime.utcnow()}"'))
+    print(last_function, datetime.datetime.utcnow())
     if messages:
         return messages
     else:
@@ -43,4 +44,4 @@ def main_function():
 while True:
     time.sleep(10)
     main_function()
-    last_function = datetime.datetime.now() - datetime.timedelta(hours=2)
+    last_function = datetime.datetime.utcnow()
