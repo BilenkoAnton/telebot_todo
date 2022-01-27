ca56d822d2c8648816ce05d7aef469a2c17a13f6
BilenkoAnton <bilenko.anton@gmail.com>
2022-01-27 20:29:22 +0200

    27.01.2022
    - Optimize sql request for injection protection

--- telebot_c.py
+++ telebot_c.py
@@ -1,25 +1,30 @@
-import time
 import datetime
+import sqlite3
+from os import environ
+from time import sleep
 
 import telebot
-import sqlite3
 
-TOKEN = '5132273473:AAEkJ6BWsVUXjpp5o8mfRqGnp1AsnYSBluk'
+TOKEN = environ['TELEBOT_TOKEN']
 bot = telebot.TeleBot(TOKEN)
-connection = sqlite3.connect('../todo_app/todo/db.sqlite3')
+connection = sqlite3.connect(f'../{environ["PROJECT_FOLDER"]}/todo/db.sqlite3')
 cursor = connection.cursor()
-last_function = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+last_function = f'"{datetime.datetime.utcnow() - datetime.timedelta(days=1)}"'
 
 
 def get_telegram_and_user_id_list():
-    telegram_list = list(cursor.execute(f'SELECT telegram_id, usr_id from todo_app_account'))
+    telegram_list = list(cursor.execute('SELECT telegram_id, usr_id from todo_app_account'))
     return telegram_list
 
 
 def get_messages(user_id):
-    messages = list(cursor.execute(f'SELECT text FROM todo_app_message WHERE account_id = {user_id} AND '
-                                   f'message_date BETWEEN "{last_function}" '
-                                   f'AND "{datetime.datetime.utcnow()}"'))
+    # messages = list(cursor.execute(f'SELECT text FROM todo_app_message WHERE account_id = {user_id} AND '
+    #                                f'message_date BETWEEN "{last_function}" '
+    #                                f'AND "{datetime.datetime.utcnow()}"'))
+    present_time = f'"{datetime.datetime.utcnow()}"'
+    messages_request = 'SELECT text FROM todo_app_message WHERE account_id = ? AND message_date BETWEEN ? AND ?'
+    param_for_messages = (str(user_id), last_function, present_time)
+    messages = list(cursor.execute(messages_request, param_for_messages))
     if messages:
         return messages
     else:
@@ -27,7 +32,6 @@ def get_messages(user_id):
 
 
 def main_function():
-
     telegram_and_user_id_list = get_telegram_and_user_id_list()
     for telegram_and_user_id in telegram_and_user_id_list:
         telegram_id, user_id = list(telegram_and_user_id)
@@ -41,6 +45,6 @@ def main_function():
 
 
 while True:
-    time.sleep(10)
+    sleep(3)
     main_function()
-    last_function = datetime.datetime.utcnow()
+    last_function = f'"{datetime.datetime.utcnow()}"'
